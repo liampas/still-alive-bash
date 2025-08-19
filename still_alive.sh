762b481513468a0c7269d0098a3395908e19ac41
liampas <liam.plamondon@gmail.com>
2025-08-18 23:08:24 -0400

    experimented with adding multiple voices

--- still_alive.sh
+++ still_alive.sh
@@ -7,8 +7,8 @@
 #echo $test | pv -qL 200
 
 
-sed -n 6p still_alive_text.txt | tee >(espeak -ven+f3 -m -p 60 -s 180 >/dev/null 2>&1) | pv -qL 20
-#sed -n 6p still_alive_text.txt | tee >(espeak-ng -p 99 >/dev/null 2>&1) | tee >(espeak-ng -p 90 -a 50 >/dev/null 2>&1) |pv -qL 20
+#sed -n 6p text.txt | tee >(espeak -ven+f3 -m -p 80 -s 180 >/dev/null 2>&1) | pv -qL 20
+sed -n 6p text.txt | tee >(espeak -ven+f3 -m -p 80 -s 180 >/dev/null 2>&1) | tee >(espeak -ven+f3 -a 50 -m -p 82 -s 180 >/dev/null 2>&1) |pv -qL 20
 
 
 #espeak -ven+f3 -m --stdout -p 60 -s 180
