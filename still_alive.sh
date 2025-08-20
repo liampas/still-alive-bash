4b9775d048253d4d46532ffe41c375493847e185
liampas <liam.plamondon@gmail.com>
2025-08-19 20:13:19 -0400

    made it so that it draws the border at the same time as writing the text

--- still_alive.sh
+++ still_alive.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
+
 clear
+
+#mplayer -really-quiet Still_Alive_Song.mp3 >/dev/null 2>&1 &
+
+#echo -------------------------------------------------
+
 cat borders.txt
-printf '\033[30`%s\n' "My Text Goes Here"
+for line in {1..25}
+
+do 
+text=$(sed -n "${line}p" text.txt)
+length=$(sed -n "${line}p" text.txt | awk '{print length}')
+
+rest=$((47-$length))
+space=$(seq -s" " $rest|tr -d '[:digit:]')
 
 
-mplayer -really-quiet Still_Alive_Song.mp3 >/dev/null 2>&1
-
+echo -n "| " 
+echo -n "$text" | pv -qL 20 
+echo "$space|"
+#	echo "| $text$space|" | pv -qL 20
+done
